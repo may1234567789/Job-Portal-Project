5d431c7719c9505f237e60ed629ec43e6eee9b8c
Mayank Bhardwaj <mayankbhardwaj325@gmail.com>
2026-01-28 18:14:00 +0530

    Set up Sing Up and Log in pages using react usestate

--- FrontEnd/src/components/Shared/login.jsx
+++ FrontEnd/src/components/Shared/login.jsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './navbar'
+import { Label } from '@radix-ui/react-label'
+import { RadioGroup } from '@radix-ui/react-radio-group'
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import './css/signup.css'
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 function Login() {
+  const [input, setInput] = useState({
+    email: "",
+    password: "",
+    role: "",
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
+
   return (
-    <div>login</div>
+    <div>
+      <Navbar />
+      <form onSubmit={submitHandler}>
+        <div className="signup-form">
+          <h1 className=''>Log In</h1>
+          <Field>
+            <FieldLabel >Email</FieldLabel>
+            <Input
+              type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
+              placeholder="Enter your email"
+            />
+            <FieldLabel >Password</FieldLabel>
+            <Input
+              type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
+              placeholder="Enter your password"
+            />
+            <RadioGroup defaultValue="comfortable" className="w-fit flex flex-row gap-6">
+              <FieldLabel >Role</FieldLabel>
+              <div className="flex flex-row gap-6">
+                <div className="flex items-center gap-3">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role == 'student'}
+                    onChange={changeEventHandler}
+                  />
+                  <Label htmlFor="r2">Student</Label>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="admin"
+                    checked={input.role == 'admin'}
+                    onChange={changeEventHandler}
+                  />
+                  <Label htmlFor="r3">Admin</Label>
+                </div>
+              </div>
+            </RadioGroup>
+            <Button>Log In</Button>
+          </Field>
+          <span>Don't have account? <Link to="/signup">Register Now</Link></span>
+        </div>
+      </form >
+    </div>
   )
 }
 
--- FrontEnd/src/components/Shared/signup.jsx
+++ FrontEnd/src/components/Shared/signup.jsx
@@ -1,51 +1,111 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './navbar'
 import { Label } from "@/components/ui/label"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
 import { Field, FieldDescription, FieldLabel } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import './css/signup.css'
+import { Button } from '../ui/button'
+import { Link } from 'react-router-dom'
 
 function signUp() {
+  const [input, setInput] = useState({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    password : "",
+    role: "",
+    file: ""
+  });
+
+  const changeEventHandler = (e) => {
+    setInput({ ...input, [e.target.name]: e.target.value })
+  }
+
+  const changeFileHandler = (e) => {
+    setInput({ ...input, file: e.target.files?.[0] })
+  }
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log(input);
+  }
   return (
     <div>
       <Navbar />
-      <form>
+      <form onSubmit={submitHandler}>
         <div className="signup-form">
           <h1 className=''>Sign Up</h1>
           <Field>
             <FieldLabel >Name</FieldLabel>
             <Input
-              id="input-field-username"
               type="text"
+              value={input.fullName}
+              name="fullName"
+              onChange={changeEventHandler}
               placeholder="Enter your username"
             />
             <FieldLabel >Email</FieldLabel>
             <Input
               type="email"
+              value={input.email}
+              name="email"
+              onChange={changeEventHandler}
               placeholder="Enter your email"
+            />
+            <FieldLabel >Phone Number</FieldLabel>
+            <Input
+              type="number"
+              value={input.phoneNumber}
+              name="phoneNumber"
+              onChange={changeEventHandler}
+              placeholder="Enter your Phone Number"
             />
             <FieldLabel >Password</FieldLabel>
             <Input
               type="password"
+              value={input.password}
+              name="password"
+              onChange={changeEventHandler}
               placeholder="Enter your password"
             />
             <FieldLabel>Picture</FieldLabel>
-            <Input id="picture" type="file" />
+            <Input
+              id="picture"
+              type="file"
+              onChange={changeFileHandler}
+            />
 
-            <RadioGroup defaultValue="comfortable" className="w-fit">
+            <RadioGroup defaultValue="comfortable" className="w-fit flex flex-row gap-6">
               <FieldLabel >Role</FieldLabel>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="comfortable" id="r2" />
-              <Label htmlFor="r2">Student</Label>
-            </div>
-            <div className="flex items-center gap-3">
-              <RadioGroupItem value="compact" id="r3" />
-              <Label htmlFor="r3">Admin</Label>
-            </div>
-          </RadioGroup>
-        </Field>
-    </div>
+              <div className="flex flex-row gap-6">
+                <div className="flex items-center gap-3">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="student"
+                    checked={input.role == 'student'}
+                    onChange={changeEventHandler}
+                  />
+                  <Label htmlFor="r2">Student</Label>
+                </div>
+                <div className="flex items-center gap-3">
+                  <Input
+                    type="radio"
+                    name="role"
+                    value="admin"
+                    checked={input.role == 'admin'}
+                    onChange={changeEventHandler}
+                  />
+                  <Label htmlFor="r3">Admin</Label>
+                </div>
+              </div>
+            </RadioGroup>
+
+            <Button>Sign Up</Button>
+            <span>Already have an account <Link to="/login" >Login Now</Link></span>
+          </Field>
+        </div>
       </form >
     </div>
   )
